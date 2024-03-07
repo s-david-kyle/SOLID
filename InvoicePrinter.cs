13ba2f3ef87f91edf4cbed8bf8fb0df14cbfaacc
S. David Kyle <sdavidkyle@gmail.com>
2024-03-07 14:43:44 -0700

    Update AssemblyInfo version attribute

---
+++ InvoicePrinter.cs
@@ -0,0 +1,10 @@
+﻿namespace SOLID;
+
+public class InvoicePrinter
+{
+    public void PrintInvoice(Invoice invoice)
+    {
+        Console.WriteLine($"Subtotal: {invoice.Subtotal}");
+        Console.WriteLine($"Total: {invoice.CalculateTotal()}");
+    }
+}
